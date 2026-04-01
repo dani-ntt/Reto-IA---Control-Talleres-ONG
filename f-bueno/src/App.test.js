2cd9249eb904182031e13c851b88cee58108df14
dani-ntt <drodcarn@emeal.nttdata.com>
2026-04-01 14:58:36 +0200

    Delete f-bueno directory

--- f-bueno/src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import App from "./App";
-
-test("muestra el título de la aplicación de inscripciones", () => {
-  render(<App />);
-  const titleElement = screen.getByText(/Aplicación de Inscripciones/i);
-  expect(titleElement).toBeInTheDocument();
-});
